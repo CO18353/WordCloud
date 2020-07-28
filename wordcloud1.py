f960cf4c3a9133dce4cd615683a7724ae43a002a
Tanveer Singh <42859006+CO18353@users.noreply.github.com>
2020-07-28 15:59:04 +0530

    Update wordcloud1.py

--- wordcloud1.py
+++ wordcloud1.py
@@ -1,27 +1,8 @@
 import wordcloud
 import numpy as np
 from matplotlib import pyplot as plt
-from IPython.display import display
-#import fileupload
 import io
 import sys
-
-'''def _upload():
-
-    _upload_widget = fileupload.FileUploadWidget()
-
-    def _cb(change):
-        global file_contents
-        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
-        filename = change['owner'].filename
-        print('Uploaded `{}` ({:.2f} kB)'.format(
-            filename, len(decoded.read()) / 2 **10))
-        file_contents = decoded.getvalue()
-
-    _upload_widget.observe(_cb, names='data')
-    display(_upload_widget)
-
-_upload()'''
 
 def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
@@ -32,7 +13,6 @@ def calculate_frequencies(file_contents):
     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
     
-    # LEARNER CODE START HERE
     new_file=""
     for x in file_contents:
         if x not in punctuations:
